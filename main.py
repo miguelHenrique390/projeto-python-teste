f10c2f11a7ee5012119b6ed1b4407cc909211a9c
miguelHenrique390 <miguel.santos27012009@gmail.com>
2026-03-06 10:07:37 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello World!!!</h1>"
 
+@app.route("/curso")
+def curso():
+    return "<h1>Curso: Tecnico em Analise e Desenvolvimento de Sistemas</h1>"
+
 if __name__ == "__main__":
     app.run()
